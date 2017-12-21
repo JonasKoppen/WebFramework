a0ca6c076d1c238f87a73cefc66f31f329ed8693
JonasKoppen_DROS <dreaddrone@gmail.com>
2017-12-21 18:17:59 +0100

    Fix

--- server.js
+++ server.js
@@ -3,16 +3,10 @@ import { retry } from 'rxjs/operator/retry';
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const hostname = '192.168.1.40';
+const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = express();
-
-
-let apiRouter = express.Router();
-
-server.use(bodyParser.json());
-server.use('/api', apiRouter);
 
 
 server.listen(port, hostname, () => {
